0c64feb0647d01f726988ac2d8aa892775e9d009
towhaEL <37008991+towhaEL@users.noreply.github.com>
2025-03-24 23:07:41 +0600

    Time: 143 ms (90.81%) | Memory: 22.1 MB (54.80%) - LeetSync

---
+++ 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.py
@@ -0,0 +1,30 @@
+class Solution:
+    def shipWithinDays(self, nums: List[int], k: int) -> int:
+        def valid(nums, max, k):
+            c = 1
+            total = 0
+            for num in nums:
+                total += num
+                if total > max:
+                    c += 1
+                    total = num
+                    if c > k:
+                        return False
+            return True
+        
+        max = 0
+        sum = 0
+        for num in nums:
+            max = num if num>max else max
+            sum += num
+        l, r = max, sum
+        ans = 0
+        while l <= r:
+            mid = l + (r-l)//2
+            if valid(nums, mid, k):
+                ans = mid
+                r = mid-1
+            else:
+                l = mid+1
+
+        return ans
